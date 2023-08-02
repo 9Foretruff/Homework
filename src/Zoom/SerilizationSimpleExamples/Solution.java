c052b330f12128337378597bf2ea1340fb84c081
Rokitko Maksim <rokitkomaksim@gmail.com>
2023-08-02 23:30:20 +0300

    SerilizationSimpleExampels

---
+++ src/Zoom/SerilizationSimpleExamples/Solution.java
@@ -0,0 +1,80 @@
+package Zoom.SerilizationSimpleExamples;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class JavaRush implements Serializable {
+        public List<User> users = new ArrayList<>();
+    }
+}
+
+class User implements Serializable {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private boolean isMale;
+    private Country country;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public enum Country {
+        UKRAINE("Ukraine"),
+        USA("USA"),
+        OTHER("Other");
+
+        private String name;
+
+        Country(String name) {
+            this.name = name;
+        }
+
+        public String getDisplayName() {
+            return this.name;
+        }
+    }
+}
