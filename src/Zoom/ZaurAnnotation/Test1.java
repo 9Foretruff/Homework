a63787dfddc3096c2fb1ecd21d8b1a2f83319d7b
Rokitko Maksim <rokitkomaksim@gmail.com>
2023-08-12 20:38:10 +0300

    == examples

--- src/Zoom/ZaurAnnotation/Test1.java
+++ src/Zoom/ZaurAnnotation/Test1.java
@@ -4,6 +4,12 @@ public class Test1 {
     public static void main(String[] args) {
         Parent p = new Child("Maksim");
         p.showInfo();
+        Integer i1 = -127;
+        Integer i2 = -127; // Просто пример . если сравнивать Integer от -128 до 127
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i1==i2);
+        System.out.println(i1.equals(i2));
     }
 }
 
@@ -18,7 +24,6 @@ class Parent {
     void showInfo() {
         System.out.println("It's Parent class . name = " + name);
     }
-
 }
 
 class Child extends Parent {
